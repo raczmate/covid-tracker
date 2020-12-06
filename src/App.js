cf4553f3215ef322e6d46e0702d38368e87b01d5
Mate Racz <raczmate@users.noreply.github.com>
2020-12-06 20:17:03 +0100

    implemented routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { getSummary } from './api';
-
+import { MainPage, SecondPage } from './pages';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 function App() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -12,7 +18,7 @@ function App() {
           throw Error('Sikertelen api hívás');
         } else {
           setData(data);
-          console.log(data);
+          // console.log(data);
         }
         setLoading(false);
       });
@@ -23,7 +29,17 @@ function App() {
       }, 1500);
   }, [loading]);
 
-  return <div></div>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={MainPage}></Route>
+          <Route exact path="/countries" component={SecondPage}></Route>
+          <Route path="*" component={MainPage} />
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,2 @@
+export { default as MainPage } from './MainPage/MainPage';
+export { default as SecondPage } from './SecondPage/SecondPage';
