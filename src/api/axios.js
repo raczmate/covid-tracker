5f3b96ee0ac149e098b928064e15bdacd1ca72c0
Mate Racz <raczmate@users.noreply.github.com>
2020-12-07 18:07:44 +0100

    fixing calls

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,13 @@ import axios from 'axios';
 axios.defaults.headers.get['Content-Type'] = 'application/json';
 axios.defaults.headers.get['Cache-Control'] = 'no-cache';
 axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
-
+axios.defaults.headers.get('Access-Control-Allow-Origin', '*');
+axios.defaults.headers.get[('Access-Control-Allow-Credentials', 'true')];
+axios.defaults.headers.get[('Access-Control-Max-Age', '1800')];
+axios.defaults.headers.get[('Access-Control-Allow-Headers', 'content-type')];
+axios.defaults.headers.get[
+  ('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, PATCH, OPTIONS')
+];
 axios.interceptors.response.use(undefined, function (error) {
   const errObj = {
     error: error.response ? error.response.data : 'Sikertelen kapcsolódás',
